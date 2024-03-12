49ba445989a500a12a348015c4c323127e514009
saipooja-git <pallesaipooja@gmail.com>
2024-03-12 13:29:48 -0500

    new

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var product = 1;
+var factor = 1;
+factor++;
+product *= factor;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  res.send(`Product is: ${product}`);
+
 });
 
 module.exports = router;
